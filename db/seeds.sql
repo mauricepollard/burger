b2637156bd6eb665c5be4150a105d121142b7e1c
Maurice Pollard <mdpollard2@hotmail.com>
2019-05-14 21:21:20 -0400

    submit

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Monster burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Seafood burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Gladiator burger', true);
